88330d14507c7f213d84218df6457efc66c94689
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-05-17 17:57:39 +0100

    Update prowallyn.js

--- greieyn/prowallyn.js
+++ greieyn/prowallyn.js
@@ -1,7 +1,9 @@
 let prowal = [];
 let earrooProwal = [0, 0];
 
-const curLettyrStiagh = (t) => {
+const curLettyrStiagh = t => {
+    t.target.style.backgroundColor = "darkgrey";
+    setTimeout(() => t.target.style.backgroundColor = "#DDDDDD", 100);
     if (earrooProwal[1] < 5) {
         const l = t.target.textContent;
         const nahChilleen = document.getElementById(`killeen-${earrooProwal[0]}-${earrooProwal[1]}`);
@@ -10,7 +12,9 @@ const curLettyrStiagh = (t) => {
         earrooProwal[1]++;
     };
 };
-const gowLettyrMagh = () => {
+const gowLettyrMagh = t => {
+    t.target.style.backgroundColor = "darkgrey";
+    setTimeout(() => t.target.style.backgroundColor = "#DDDDDD", 100);
     if (earrooProwal[1] > 0) {
         earrooProwal[1]--;
         const roishKilleen = document.getElementById(`killeen-${earrooProwal[0]}-${earrooProwal[1]}`);
