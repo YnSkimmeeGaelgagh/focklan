4cd95222a785e6550f1f8fc377a49167c98b49db
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-06-03 20:39:22 +0100

    Update focklynRoie.js

--- greieyn/focklynRoie.js
+++ greieyn/focklynRoie.js
@@ -13,7 +13,7 @@ const focklynRoie = [
     ["argid", new Date(2024, 5, 29)],
     ["ourys", new Date(2024, 5, 30)],
     ["paays", new Date(2024, 5, 31)],
-    ["niart", new Date(2024, 6, 3)],
+    ["niart", new Date(2024, 6, 1)],
     ["dhone", new Date(2024, 6, 2)]
     // ["flurt", new Date(2024, 6, 3)],
     // ["heose", new Date(2024, 6, 4)],
