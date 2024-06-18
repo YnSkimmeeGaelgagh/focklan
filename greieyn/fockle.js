f8129dd8a842cc49fb42a9944d1ee1382e89c378
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-06-18 23:27:38 +0100

    Update fockle.js

--- greieyn/fockle.js
+++ greieyn/fockle.js
@@ -6,7 +6,7 @@ const reihFockle = () => {
     return rolleyFocklynQueigLetteryn[r];
 };
 // console.log(reihFockle());
-let fockle = "moyrn";
+let fockle = "garey";
 const lettyryn = fockle.split("");
 let earrooLettyryn = {};
 lettyryn.forEach(l => {
